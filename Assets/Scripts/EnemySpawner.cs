7399087d495c671e0b1e640a06b1d7971b01acdb
J4rpp1 <jaka765@gmail.com>
2022-04-08 13:45:49 +0300

    test

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,11 +5,13 @@ using TMPro;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public static EnemySpawner instance;
+
     MouseFollow mouseFollow;
     public TMP_Text bossText;
+    public TMP_Text waveText;
     public TMP_Text countText;
     public GameObject shopMenu;
-    public static EnemySpawner instance;
     public GameObject[] mediumSpawnPoints;
     public GameObject bossSpawnPoint;
     public GameObject littleSpawnPoint;
@@ -21,6 +23,8 @@ public class EnemySpawner : MonoBehaviour
     public int enemiesKilled;
     public int bossesKilled;
     public bool enableBossText;
+    public bool shopOpen;
+    public bool roundStarted;
 
 
     public bool completedWave1;
@@ -32,6 +36,8 @@ public class EnemySpawner : MonoBehaviour
     }
     void Start()
     {
+        waveText.text = "Wave 1";
+        wave = 1;
         mouseFollow = FindObjectOfType<MouseFollow>();
         Cursor.visible = false;
         enableBossText = true;
@@ -45,6 +51,13 @@ public class EnemySpawner : MonoBehaviour
         {
             StartCoroutine(Boss());
         }
+
+        if (enemiesKilled == 16 && enableBossText && wave == 2)
+        {
+            StartCoroutine(Boss());
+        }
+
+
         if (bossesKilled == 1)
         {
             completedWave1 = true;
@@ -52,8 +65,14 @@ public class EnemySpawner : MonoBehaviour
             mouseFollow.hideCrosshair = true;
             shopMenu.SetActive(true);
             Cursor.visible = true;
+            bossesKilled = 0;
         }
-
+        if (wave == 2 && !roundStarted)
+        {
+            
+            waveText.text = "Wave " + wave.ToString();
+            StartCoroutine(Enemyspawner2());
+        }
     }
 
     #region Wave system
@@ -108,6 +127,72 @@ public class EnemySpawner : MonoBehaviour
         yield return new WaitForSeconds(2);
 
         
+    }
+
+    IEnumerator Enemyspawner2()
+    {
+        shopMenu.SetActive(false);
+        roundStarted = true;
+        int spawn = Random.Range(0, mediumSpawnPoints.Length); //sets random spawn point
+        int select = Random.Range(0, mediumEnemy.Length);      //selects ice or fire enemy
+        GameObject.Instantiate(mediumEnemy[select], mediumSpawnPoints[spawn].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+
+        int spawn2 = Random.Range(0, mediumSpawnPoints.Length);
+        int select2 = Random.Range(0, mediumEnemy.Length);
+        GameObject.Instantiate(mediumEnemy[select2], mediumSpawnPoints[spawn2].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        int spawn3 = Random.Range(0, mediumSpawnPoints.Length);
+        int select3 = Random.Range(0, mediumEnemy.Length);
+        GameObject.Instantiate(mediumEnemy[select3], mediumSpawnPoints[spawn3].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        int select10 = Random.Range(0, littleEnemy.Length);
+        GameObject.Instantiate(littleEnemy[select10], littleSpawnPoint.transform.position, Quaternion.identity);  //Spawnaa pieni vihollinen
+
+        int spawn4 = Random.Range(0, mediumSpawnPoints.Length);
+        int select4 = Random.Range(0, mediumEnemy.Length);
+        GameObject.Instantiate(mediumEnemy[select4], mediumSpawnPoints[spawn4].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select], mediumSpawnPoints[spawn].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select3], mediumSpawnPoints[spawn3].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select2], mediumSpawnPoints[spawn2].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select4], mediumSpawnPoints[spawn4].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(3);
+
+        int select8 = Random.Range(0, littleEnemy.Length);
+        GameObject.Instantiate(littleEnemy[select8], littleSpawnPoint.transform.position, Quaternion.identity);  //Spawnaa pieni vihollinen
+
+        int spawn5 = Random.Range(0, mediumSpawnPoints.Length);
+        int select5 = Random.Range(0, mediumEnemy.Length);
+        GameObject.Instantiate(mediumEnemy[select5], mediumSpawnPoints[spawn5].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select3], mediumSpawnPoints[spawn3].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        int spawn6 = Random.Range(0, mediumSpawnPoints.Length);
+        int select6 = Random.Range(0, mediumEnemy.Length);
+        GameObject.Instantiate(mediumEnemy[select6], mediumSpawnPoints[spawn6].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        GameObject.Instantiate(mediumEnemy[select2], mediumSpawnPoints[spawn2].transform.position, Quaternion.identity);
+
+        GameObject.Instantiate(mediumEnemy[select], mediumSpawnPoints[spawn].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+
+        int select9 = Random.Range(0, littleEnemy.Length);
+        GameObject.Instantiate(littleEnemy[select9], littleSpawnPoint.transform.position, Quaternion.identity);
+
+
     }
     #endregion
     IEnumerator Boss()
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    EnemySpawner enemySpawner;
+    Score score;
+
+    public float bombs;
+
+    private void Awake()
+    {
+       
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+        score = FindObjectOfType<Score>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(bombs);
+    }
+
+    public void BuyBomb()
+    {
+        if(score.currentScore > 500)
+        {
+            score.currentScore = score.currentScore - 500;
+            bombs = bombs + 1;
+        }
+        
+    }
+
+    public void NextWave()
+    {
+        enemySpawner.wave = enemySpawner.wave + 1;
+    }
+}
