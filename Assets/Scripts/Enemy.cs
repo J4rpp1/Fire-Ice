044ba75eebff3a175ea65f5196055bcabc591746
Spazi <talishmarprkle+g@gmail.com>
2022-04-26 08:15:41 +0300

    Enemy sounds. Music slider position. Music starts muted. MainCamera tag.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,124 +4,140 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public bool isFireEnemy;
-    public bool isFlyingEnemy;
-    EnemySpawner enemySpawner;
-    Score score;
-    PlayerHp playerHp;
-    public float speed = 0.7f;
-    public Transform target;
-    public int damage = 2;
-    public float maxHp = 5;
-    public float currentHp;
-    public float takeDamage = 1;
-    public float addPoints = 100;
-    public int addToKills = 1;
-    private bool canDamage;
+	[SerializeField] AudioClip hurtSound;
+	[SerializeField] AudioClip blockSound;
+	public bool isFireEnemy;
+	public bool isFlyingEnemy;
+	EnemySpawner enemySpawner;
+	Score score;
+	PlayerHp playerHp;
+	public float speed = 0.7f;
+	public Transform target;
+	public int damage = 2;
+	public float maxHp = 5;
+	public float currentHp;
+	public float takeDamage = 1;
+	public float addPoints = 100;
+	public int addToKills = 1;
+	private bool canDamage;
+
+	Vector3 soundPosition;
 
 
 
-    private void Awake()
-    {
-        playerHp = FindObjectOfType<PlayerHp>();
-        enemySpawner = FindObjectOfType<EnemySpawner>();
-        score = FindObjectOfType<Score>();
-    }
+	private void Awake()
+	{
+		playerHp = FindObjectOfType<PlayerHp>();
+		enemySpawner = FindObjectOfType<EnemySpawner>();
+		score = FindObjectOfType<Score>();
+	}
 
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (isFireEnemy)
-        {
-            if (other.name == "Player" && canDamage)
-            {
-                //playerHp.currentHp -= Time.deltaTime * damage / 6f;
-                StartCoroutine(Damage());
-            }
-            if (other.tag == "FireBullet")
-            {
-                currentHp = currentHp + takeDamage;
-            }
-            if (other.tag == "IceBullet")
-            {
-                currentHp = currentHp - takeDamage;
-            }
-        }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (isFireEnemy)
+		{
+			if (other.name == "Player" && canDamage)
+			{
+				//playerHp.currentHp -= Time.deltaTime * damage / 6f;
+				StartCoroutine(Damage());
+			}
+			if (other.tag == "FireBullet")
+			{
+				EnemyHurt(false);
+			}
+			if (other.tag == "IceBullet")
+			{
+				EnemyHurt(true);
+			}
+		}
 
-        if (!isFireEnemy)
-        {
-            if (other.name == "Player" && canDamage)
-            {
-                //playerHp.currentHp -= Time.deltaTime * damage / 6f;
-                StartCoroutine(Damage());
-            }
-            if (other.tag == "FireBullet")
-            {
-                currentHp = currentHp - takeDamage;
-            }
-            if (other.tag == "IceBullet")
-            {
-                currentHp = currentHp + takeDamage;
-            }
-        }
+		if (!isFireEnemy)
+		{
+			if (other.name == "Player" && canDamage)
+			{
+				//playerHp.currentHp -= Time.deltaTime * damage / 6f;
+				StartCoroutine(Damage());
+			}
+			if (other.tag == "FireBullet")
+			{
+				EnemyHurt(true);
+			}
+			if (other.tag == "IceBullet")
+			{
+				EnemyHurt(false);
+			}
+		}
 
-        if (other.tag == "Bomb")
-            currentHp = 0;
+		if (other.tag == "Bomb")
+			currentHp = 0;
 
-    }
+	}
 
-    void Start()
-    {
-        canDamage = true;
-        if(isFlyingEnemy)
-        {
-            target = GameObject.FindWithTag("FlyingTarget").transform;
-        }
-        else
-        {
-            target = GameObject.FindWithTag("target").transform;
-           
-        }
-        
-        currentHp = maxHp;
+	void Start()
+	{
+		canDamage = true;
+		if (isFlyingEnemy)
+		{
+			target = GameObject.FindWithTag("FlyingTarget").transform;
+		}
+		else
+		{
+			target = GameObject.FindWithTag("target").transform;
 
+		}
 
+		currentHp = maxHp;
 
-
-    }
+		soundPosition = Camera.main.transform.position;
 
 
-    void Update()
-    {
-
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        // Debug.Log(currentHp);
-        if (currentHp > maxHp)
-        {
-            currentHp = maxHp;
-        }
-
-        if (currentHp == 0 && canDamage)
-        {
-            enemySpawner.enemiesKilled = enemySpawner.enemiesKilled + addToKills;
-            score.currentScore = score.currentScore + addPoints;
-            Destroy(gameObject);
-
-        }
+	}
 
 
-    }
+	void Update()
+	{
+
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		// Debug.Log(currentHp);
+		if (currentHp > maxHp)
+		{
+			currentHp = maxHp;
+		}
+
+		if (currentHp == 0 && canDamage)
+		{
+			enemySpawner.enemiesKilled = enemySpawner.enemiesKilled + addToKills;
+			score.currentScore = score.currentScore + addPoints;
+			Destroy(gameObject);
+
+		}
 
 
-    IEnumerator Damage()
-    {
-        enemySpawner.enemiesKilled = enemySpawner.enemiesKilled + addToKills;
-        playerHp.hurtSound.Play();
-        canDamage = false;
-        playerHp.currentHp = playerHp.currentHp - damage;
-        yield return new WaitForSeconds(0.1f);
-        Destroy(gameObject);
+	}
 
-    }
+	void EnemyHurt(bool hurt)
+	{
+		if (hurt)
+		{
+			AudioSource.PlayClipAtPoint(hurtSound, transform.position);
+			currentHp = currentHp - takeDamage;
+		}
+		else
+		{
+			AudioSource.PlayClipAtPoint(blockSound, transform.position);
+			currentHp = currentHp + takeDamage;
+		}
+	}
+	IEnumerator Damage()
+	{
+		enemySpawner.enemiesKilled = enemySpawner.enemiesKilled + addToKills;
+		playerHp.hurtSound.Play();
+		canDamage = false;
+		playerHp.currentHp = playerHp.currentHp - damage;
+		yield return new WaitForSeconds(0.1f);
+		Destroy(gameObject);
+
+	}
 }
--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -5,16 +5,20 @@ using UnityEngine.Audio;
 
 public class SetVolume : MonoBehaviour
 {
-    public AudioMixer musicMixer;
-    public AudioMixer fxMixer;
+	public AudioMixer musicMixer;
+	public AudioMixer fxMixer;
 
-    public void SetMusicLevel (float sliderValue)
-    {
-        musicMixer.SetFloat("MusicVol", Mathf.Log10 (sliderValue) *20);
-    }
+	void Start()
+	{
+		SetMusicLevel(0f);
+	}
+	public void SetMusicLevel(float sliderValue)
+	{
+		musicMixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+	}
 
-    public void SetFxLevel(float sliderValue)
-    {
-        fxMixer.SetFloat("FxVolume", Mathf.Log10(sliderValue) * 20);
-    }
+	public void SetFxLevel(float sliderValue)
+	{
+		fxMixer.SetFloat("FxVolume", Mathf.Log10(sliderValue) * 20);
+	}
 }
