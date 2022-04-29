e416a029f9908f5bd02704bf6aedfa61af8bfcd8
Pazi <talishmarprkle+g@gmail.com>
2022-04-29 10:29:51 +0300

    Flying monster sorting order fix

---
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	public Transform lookTarget;
+
+	void Start()
+	{
+		if (!lookTarget)
+			lookTarget = Camera.main.transform;
+	}
+	void LateUpdate()
+	{
+		transform.LookAt(lookTarget, Vector3.up);
+	}
+}
